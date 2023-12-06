c4774496a507fc7d0f419dd34b757c51f1913077
gabriel-cecil <152939852+gabriel-cecil@users.noreply.github.com>
2023-12-06 18:34:22 -0300

    Add files via upload

---
+++ Bookstore_API/Bookstore.Web/Services/Contracts/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿using Bookstore.Web.Models;
+
+namespace Bookstore.Web.Services.Contracts
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<CategoryViewModel>> GetAllCategories(string token);
+    }
+}
